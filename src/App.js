d07cd5cafdb297487bbdd5d02b0055afcab541ba
JahedHossenBangladesh <jahedhossen4358.com>
2021-01-25 21:16:30 +0600

    this is the styled-component and react-icons is testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FaAdn } from "react-icons/fa";
+import styled from "styled-components";
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div>
-        <h1>This is Jahdded</h1>;
+        <h1>
+          This is Jahdded <FaAdn />
+          <Buttons>This </Buttons>{" "}
+        </h1>
+        ;
       </div>
     );
   }
 }
+const coolor = "#f15025";
+const Buttons = styled.button`
+  color: red;
+  background: ${coolor};
+  font-size: ${(props) => (props.large ? "3rem" : "1.5rem")};
+`;
+export default App;
